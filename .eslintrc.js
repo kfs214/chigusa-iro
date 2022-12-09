41ccf9b03294a8d23b4f7881ae898b2eb2d1a252
kazukiUshijima <info@kfs214.net>
2022-12-09 18:01:49 +0900

    chore: set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  extends: ['standard-with-typescript', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+  },
+};
