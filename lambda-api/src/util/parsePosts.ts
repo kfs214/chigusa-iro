ddb08ef9b390f1e685308ea9bf412a4872fb13f1
kazukiUshijima <info@kfs214.net>
2023-06-14 23:24:07 +0900

    feat: decodeURI

--- lambda-api/src/util/parsePosts.ts
+++ lambda-api/src/util/parsePosts.ts
@@ -1,12 +1,23 @@
 import { decode } from "html-entities";
 import { Post, WPPost } from "@/type";
 
+function formatURI(uri: string) {
+  try {
+    return decodeURI(uri);
+  } catch (e) {
+    return uri;
+  }
+}
+
 function parsePost([post]: WPPost[]): Post {
   const { link, title, excerpt } = post;
+
+  const decodedLink = formatURI(link);
+
   const excerptText = excerpt.rendered.replace(/<.*?>/g, "");
   const decodedExcerpt = decode(excerptText);
 
-  return { link, title: title.rendered, excerpt: decodedExcerpt };
+  return { link: decodedLink, title: title.rendered, excerpt: decodedExcerpt };
 }
 
 export function parsePosts(posts: Array<WPPost[] | undefined>): Post[] {
